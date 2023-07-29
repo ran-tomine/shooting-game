7ea98fb57cedc2f6e7e298552371b1e7edbf5fbf
ran02160 <ran02160ay@gmail.com>
2023-07-29 09:36:48 +0900

    地球の追加

--- shooting-game/GameScene.swift
+++ shooting-game/GameScene.swift
@@ -9,8 +9,13 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
+    var earth: SKSpriteNode!
     override func didMove(to view: SKView) {
-        
+        self.earth = SKSpriteNode(imageNamed: "earth")
+        self.earth.xScale = 1.5
+        self.earth.yScale = 0.3
+        self.earth.position = CGPoint(x: 0, y: -frame.height / 2)
+        self.earth.zPosition = -1.0
+        addChild(self.earth)
     }
 }
